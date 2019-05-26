86fdcbc60c55392704f4878a511f8aa91af5afb8
letap1121 <sagar2332@icloud.com>
2019-05-26 14:06:39 -0500

    Add files via upload

---
+++ Collection Classes in C#  and More WPF/VendingMachine/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VendingMachineExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declare VendingMachine reference variable.
+            VendingMachine vm;
+
+            // Instantiate VendingMachine object.
+            vm = new VendingMachine();
+
+            // Test ToString method of vm.
+            Console.WriteLine(vm.ToString());
+
+            
+            // Try to buy 6 candy bars.  vm has only five candy 
+            // bars, so the 6th time will not be successful.
+            // Each candy bar requires 3 quarters.
+            for (int i = 1; i <= 6; i++) 
+            {
+                for (int j = 1; j <= 3; j++)
+                {
+                    Console.WriteLine(vm.DepositQuarter());
+                }
+                Console.WriteLine(vm.SelectCandy());
+                Console.WriteLine(vm.ToString());
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Collection Classes in C#  and More WPF/VendingMachine/VendingMachine.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VendingMachineExample
+{
+    /// <summary>          
+    /// Simulate a candy bar vending machine that starts with
+    /// 5 candy bars, where each candy bar costs 3 quarters.
+    /// </summary>
+
+    public class VendingMachine
+    {
+        // private instance variables
+        private int _numQuarters, _numCandyBars;
+
+        // Public noarg constructor
+        public VendingMachine()
+        {
+            // Set the candy bar cost in quarters.  
+            _numQuarters = 3;
+
+            // Set initial number of candy bars.
+            _numCandyBars = 5;
+        }
+
+        // Public read only properties:
+
+        public int NumQuarters
+        {
+            get { return _numQuarters; }
+        }
+
+        public int NumCandyBars
+        {
+            get { return _numCandyBars; }
+        }
+
+        // Public methods:
+
+        public string DepositQuarter()
+        {
+            _numQuarters++;
+            return "Quarter deposited.";
+        }
+
+        public string SelectCandy()
+        {
+            if (_numQuarters >= 3 && _numCandyBars > 0)
+            {
+                _numQuarters = _numQuarters -3;
+                _numCandyBars--;
+                return "Candy bar dispensed";
+            }
+            else if (_numCandyBars > 0)
+            {
+                return "Not enough quarters to buy candy bar.";
+            }
+            else
+            {
+                return "No candy bars in machine.";
+            }
+        }
+
+        public override string ToString()
+        {
+
+            return "Total Quarters: " + _numQuarters + " Total Bars:" + _numCandyBars;
+        }
+    }
+
+}
