10c7b9153531d7f0701a825042d3fe6eb75dfd1e
letap1121 <sagar2332@icloud.com>
2019-05-26 13:20:07 -0500

    Add files via upload

---
+++ Counsole and C# Fundamental/Speeder/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Speeder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string _input;
+            int _speed = 0;
+            Console.Write("Please enter your speed:");
+            _input = Console.ReadLine();
+            if (int.TryParse(_input, out _speed))
+            {
+                if (_speed <= 30)
+                {
+                    Console.WriteLine("You are going slow.");
+                }
+                else if (_speed > 30 && _speed <= 55)
+                {
+                    Console.WriteLine("You are going normal speed.");
+                }
+                else if (_speed > 55 && _speed < 100)
+                {
+                    Console.WriteLine("You are going too fast.");
+                }
+                else
+                {
+                    Console.WriteLine("You're crazy!");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Not a valid number.");
+            }
+            Console.WriteLine("Press enter to continue.");
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Counsole and C# Fundamental/ValidNumber/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValidNumber2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int _validNumber = -1;
+            string _input = "";
+            //Why is this flag here?  What happens when an invalid number is entered into TryParse
+            bool _userNotDone = true;
+
+            do
+            {
+                Console.Write("Please enter a number greater than or equal to 0:");
+                _input = Console.ReadLine();
+                if (!int.TryParse(_input, out _validNumber))
+                {
+                    Console.WriteLine("Sorry. That is not a valid number. Please try again.");
+                }
+                else if (_validNumber < 0)
+                {
+                    Console.WriteLine("Sorry. Number must be greater than or equal to 0.");
+                }
+                else
+                {
+                    _userNotDone = false;
+                }
+            }
+            while (_userNotDone == true || _validNumber < 0) ;
+
+            Console.WriteLine("You entered {0}", _validNumber);
+            Console.WriteLine("Press enter to continue.");
+            Console.ReadLine();
+        }
+    }
+}
