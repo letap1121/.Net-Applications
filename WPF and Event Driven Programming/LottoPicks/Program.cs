8ff3f63d37c43ea00e3289721fe6cfbf7e378172
letap1121 <sagar2332@icloud.com>
2019-05-26 13:47:29 -0500

    Add files via upload

---
+++ WPF and Event Driven Programming/LottoPicks/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LottoPicks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello! Welcome to Pick 7 Lotto! Your lucky numbers are:");
+            Urn urn = new Urn(10);
+            //Urn urn2 = new Urn(100);
+            //Urn urn3 = new Urn(200);
+            
+            for (int i = 0; i < 8; i++)
+                Console.WriteLine(urn.Draw());
+            Console.WriteLine("Press any key to continue.");
+            Console.ReadLine();
+        }
+    }
+}
