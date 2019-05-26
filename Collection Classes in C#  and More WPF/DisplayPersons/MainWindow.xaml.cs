c558e45aaf8da4c2a41ae71bbf1ea66d858678b9
letap1121 <sagar2332@icloud.com>
2019-05-26 13:54:52 -0500

    Add files via upload

---
+++ Collection Classes in C#  and More WPF/DisplayPersons/MainWindow.xaml.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Collections;
+
+namespace DisplayPersons
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+
+  
+    public partial class MainWindow : Window
+    {
+        // List containing Person Objects
+        private List<Person> col = null;
+        public MainWindow()
+        {
+            InitializeComponent();
+            col = new List<Person>();
+        }
+
+        private void btnAddPerson_Click(object sender, RoutedEventArgs e)
+        {
+            int age=0;
+            if(!String.IsNullOrEmpty(txtName.Text) && Int32.TryParse(txtAge.Text, out age))
+            {
+                // Obtain gender.
+                char gender;
+                if (radF.IsChecked.Value)
+                {
+                    gender = 'F';
+                }
+                else
+                {
+                    gender = 'M';
+                }
+
+                // Instantiate new person object with information from form.
+                Person p = new Person(txtName.Text,
+                    gender, age);
+
+                // Add new Person object to collection.
+                col.Add(p);
+
+                // Add name of new Person object to combobox.
+                cboPerson.Items.Add(p.Name);
+
+                txtAge.Text = "";
+                txtName.Text = "";
+                radF.IsChecked = false;
+                radM.IsChecked = false;
+                cboPerson.SelectedIndex = -1;
+
+            }
+            else
+            {
+                MessageBox.Show("Couldn't add person.  Check values.");
+            }
+        }
+
+        private void cboPerson_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            // Find index of selected person in combo box.
+            int i = cboPerson.SelectedIndex;
+
+            if (i >= 0)
+            {
+                // Set reference to selected person in combo box.
+                Person p = col[i];
+
+                // Display person data on form.
+                txtName.Text = p.Name;
+                if (p.Gender == 'M')
+                {
+                    radM.IsChecked = true;
+                }
+                else
+                {
+                    radF.IsChecked = true;
+                }
+                txtAge.Text = p.Age.ToString();
+            }
+        }
+
+        private void bthDeletePerson_Click(object sender, RoutedEventArgs e)
+        {
+            // Find index of selected person in combo box.
+            int i = cboPerson.SelectedIndex;
+
+            if (i >= 0)
+            {
+                // Remove name of selected person from combo box.
+                cboPerson.Items.RemoveAt(i);
+
+                // Remove selected person from collection.
+                col.RemoveAt(i);
+
+                // Clear person data from form.
+                txtName.Clear();
+                txtAge.Text = "";
+                radF.IsChecked = false;
+                radM.IsChecked = false;
+
+                // No name should be selected in combobox.
+                cboPerson.SelectedIndex = -1;
+            }
+        }
+
+        private void btnHaveBirthday_Click(object sender, RoutedEventArgs e)
+        {
+            // Find index of selected person in combo box.
+            int i = cboPerson.SelectedIndex;
+
+            // Exit method if no person is selected.
+            if (i < 0)
+            {
+                return;
+            }
+
+            // Set reference to selected person in combo box.
+            Person p = col[i];
+
+            // Call HaveBirthday method.
+            p.HaveBirthday();
+
+            // Display new person data on form.
+            txtName.Text = p.Name;
+            if (p.Gender == 'M')
+            {
+                radM.IsChecked = true;
+            }
+            else
+            {
+                radF.IsChecked = true;
+            }
+            txtAge.Text = p.Age.ToString();
+        }
+    }
+}
---
+++ Collection Classes in C#  and More WPF/DisplayPersons/Person.cs
@@ -0,0 +1,98 @@
+﻿
+/// <summary>
+/// Person class with properties Name, Gender, and Age
+/// Methods are HaveBirthday and ToString
+/// </summary>
+/// 
+
+
+public class Person
+{
+    // Instance variables
+    private string _name;
+    private char _gender;
+    private int _age;
+
+    // Constructors
+    public Person()
+    {
+        _name = "Unknown";
+        _gender = 'U';
+        _age = -1;
+    }
+
+    public Person(string theName, char theGender, int theAge)
+    {
+        // Name cannot be the empty string.
+        if (theName != "")
+        {
+            _name = theName;
+        }
+        else
+        {
+            _name = "Unknown";
+        }
+
+        if (theGender == 'F' || theGender == 'M' || theGender=='U')
+        {
+            _gender = theGender;
+        }
+        else
+        {
+            _gender = 'U';
+        }
+
+        // Age must be nonnegative.  -1 means illegal age.
+        if (theAge >= 0)
+        {
+            _age = theAge;
+        }
+        else
+        {
+            _age = -1;
+        }
+    }
+
+    // Read only property
+    public string Name
+    {
+        get { return _name; }
+    }
+
+    // Read only property
+    public char Gender
+    {
+        get { return _gender; }
+    }
+
+    // Read/write property
+    public int Age
+    {
+        get { return _age; }
+
+        set
+        {
+            if (value >= 0)
+            {
+                _age = value;
+            }
+            else
+            {
+                _age = -1;
+            }
+        }
+    }
+
+    public void HaveBirthday()
+    {
+        if (_age >= 0)
+        {
+            _age++;
+        }
+    }
+
+    public override string ToString()
+    {
+        return "Name " + _name + "Gender: " + _gender + "Age: " + _age;
+    }
+}
