0e75157a3855bb5674f9b3e0c6a7605b24604407
letap1121 <sagar2332@icloud.com>
2019-05-26 14:01:08 -0500

    Add files via upload

---
+++ Collection Classes in C#  and More WPF/QueueExample/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueueExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Initialize Queue
+            Queue<string> queue = new Queue<string>();
+            //Add Item to Queue
+            queue.Enqueue("A");
+            queue.Enqueue("B");
+            queue.Enqueue("C");
+            //Remove item from Queue
+            Console.WriteLine(queue.Dequeue());
+            queue.Enqueue("D");
+            Console.WriteLine(queue.Dequeue());
+            Console.WriteLine(queue.Dequeue());
+            queue.Enqueue("E");
+            Console.WriteLine(queue.Dequeue());
+            Console.WriteLine(queue.Dequeue());
+
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
