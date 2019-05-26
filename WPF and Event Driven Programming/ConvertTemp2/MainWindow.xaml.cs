7def0988eff20fc4d3ecfbb71472b2a1e6849c1e
letap1121 <sagar2332@icloud.com>
2019-05-26 13:42:13 -0500

    Add files via upload

---
+++ WPF and Event Driven Programming/ConvertTemp2/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ConvertTemp2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConvert_Click(object sender, RoutedEventArgs e)
+        {
+            int cel = 0, fahr;
+
+            if (int.TryParse(txtCel.Text, out cel))
+            {
+                fahr = 9 * cel / 5 + 32;
+                txtFahr.Text = fahr.ToString();
+            }
+            else
+            {
+                txtFahr.Text = "NA";
+            }
+        }
+    }
+}
