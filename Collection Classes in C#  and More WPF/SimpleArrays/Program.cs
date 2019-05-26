5e5fdf023f5144a15e8c9174516b2c319d688037
letap1121 <sagar2332@icloud.com>
2019-05-26 14:02:48 -0500

    Add files via upload

---
+++ Collection Classes in C#  and More WPF/SimpleArrays/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Initialize an array
+            int[] items = new int[3];
+            //Set each item in the array
+            items[0] = 5;
+            items[1] = 2;
+            items[2] = 7;
+            //What would happen if you uncomment the next line?
+            //items[3] = 9;
+
+            int total=0;
+            foreach (int x in items)
+                total = total + x;
+
+            Console.WriteLine("Total adding all items in the array: " + total);
+            //Arrays are a type in C#
+            Console.WriteLine("Items in array: " + items.Count());
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Collection Classes in C#  and More WPF/SimpleArrays/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SimpleArrays")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SimpleArrays")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("86844c87-44dd-4d91-85f7-5c1d1b7cb3bf")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
