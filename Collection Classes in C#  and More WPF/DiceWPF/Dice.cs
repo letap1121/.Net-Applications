9ee3c573ae1c4e41ef38ff1be6c3de8504005968
letap1121 <sagar2332@icloud.com>
2019-05-26 13:53:51 -0500

    Add files via upload

---
+++ Collection Classes in C#  and More WPF/DiceWPF/Dice.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceWPF
+{
+    public class Dice
+    {
+        // private instance variables.
+        private int _face1, _face2;
+        private Random r = new Random();
+
+        // Noarg constructor
+        // Newly created die object has a random value
+        public Dice()
+        {
+            Roll();
+        }
+
+       
+
+        // Read only properties
+        public int Face1
+        {
+            get { return _face1; }
+        }
+
+        public int Face2
+        {
+            get { return _face2; }
+        }
+
+        // Public method
+        public void Roll()
+        {
+            _face1 = r.Next(1, 7);
+            _face2 = r.Next(1, 7);
+        }
+    }
+}
---
+++ Collection Classes in C#  and More WPF/DiceWPF/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DiceWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        // Use Dice object to obtain results of dice rolls.
+        private Dice dice = new Dice();
+
+        // Create an array of face to contain images.
+        // Only the indices 1, 2, 3, 4, 5, 6 are used (not 0).
+        BitmapImage[] face = new BitmapImage[7];
+
+        public MainWindow()
+        {
+            
+            InitializeComponent();
+
+            for (int i = 1; i <= 6; i++)
+            {
+                face[i] = new BitmapImage(new Uri("Images/" + i.ToString() +".jpg", UriKind.Relative));
+            }
+
+            imgDie1.Source = face[1];
+            imgDie2.Source = face[1];
+        }
+
+        private void btnRoll_Click(object sender, RoutedEventArgs e)
+        {
+            // Roll dice.
+            dice.Roll();
+
+            // Display values of dice in Image Control.
+            imgDie1.Source = face[dice.Face1];
+            imgDie2.Source = face[dice.Face2];
+        }
+    }
+}
