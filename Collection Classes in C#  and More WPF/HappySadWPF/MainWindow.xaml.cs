29f86093cc197c72b6c8711a9e5b6b40e6206119
letap1121 <sagar2332@icloud.com>
2019-05-26 13:57:53 -0500

    Add files via upload

---
+++ Collection Classes in C#  and More WPF/HappySadWPF/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HappySadWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void imgFace_MouseEnter(object sender, MouseEventArgs e)
+        {
+            imgFace.Source = new BitmapImage(new Uri("/Images/happy.jpg", UriKind.Relative));
+        }
+
+        private void imgFace_MouseLeave(object sender, MouseEventArgs e)
+        {
+            imgFace.Source = new BitmapImage(new Uri("/Images/sad.jpg", UriKind.Relative));
+        }
+
+        private void imgFace_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.Show("It worked!!");
+        }
+
+        private void imgFace_MouseRightButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.Show("Right mouse button It worked!!");
+        }
+    }
+}
