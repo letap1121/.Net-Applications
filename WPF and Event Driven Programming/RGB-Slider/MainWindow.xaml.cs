f2e9fccbe971d5cb4182bc437762d4ba50e51049
letap1121 <sagar2332@icloud.com>
2019-05-26 13:49:02 -0500

    Add files via upload

---
+++ WPF and Event Driven Programming/RGB-Slider/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RGB_Slider
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            ChangeColor();
+        }
+
+        //How can we optimize this code?
+
+        private void SliderValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            ChangeColor();
+        }
+
+        private void sldRed_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            ChangeColor();
+        }
+
+        private void sldGreen_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            ChangeColor();
+        }
+
+        private void sldBlue_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            ChangeColor();
+        }
+
+        void ChangeColor()
+        {
+            Color color = Color.FromArgb(255, Convert.ToByte(sldRed.Value),Convert.ToByte(sldGreen.Value), Convert.ToByte(sldBlue.Value));
+            rctColorArea.Fill = new SolidColorBrush(color);
+
+            double interest = sldBlue.Value;
+        }
+    }
+}
