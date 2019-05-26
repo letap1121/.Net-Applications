292e35fea8e68216c5ca67d3c7bdf28782b94e00
letap1121 <sagar2332@icloud.com>
2019-05-26 13:58:39 -0500

    Add files via upload

---
+++ Collection Classes in C#  and More WPF/ListExample/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>();
+            numbers.Add(3);
+            List<string> cities = new List<string>();
+            
+            cities.Add("Chicago");
+            cities.Add("San Diego");
+            cities.Add("Atlanta");
+            cities.Add("Boston");
+            cities.Add("Seattle");
+            cities.Add("Dallas");
+            cities.Add("Kalamazoo");
+
+            Console.WriteLine("\nView the List");
+            foreach (string city in cities)
+            {
+                Console.WriteLine(city);
+            }
+
+            Console.WriteLine("\nView the List Sorted.");
+            cities.Sort();
+            foreach (string city in cities)
+            {
+                Console.WriteLine(city);
+            }
+
+            Console.WriteLine("\nView the List Reversed.");
+            cities.Reverse();
+            foreach (string city in cities)
+            {
+                Console.WriteLine(city);
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
