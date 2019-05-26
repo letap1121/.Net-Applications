9fecd0bac5e06d5adb7e6311a50ab43174b5cff5
letap1121 <sagar2332@icloud.com>
2019-05-26 13:46:18 -0500

    Add files via upload

---
+++ WPF and Event Driven Programming/Inventory/Product.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory 
+{
+    //This example is based off an example in your book with modifications to the Event Signature.
+    public delegate void PriceChangedEventHandler(decimal oldPrice, decimal newPrice);
+
+    class Product
+    {
+        private string _name;
+        private decimal _price;
+
+        
+
+        public event PriceChangedEventHandler PriceChanged;
+       
+        public Product()
+        {
+            _name = "";
+            _price = 0;
+        }
+
+        public Product (string name)
+        {
+            _name = name;
+            _price = 0;
+        }
+
+        public Product (string name, decimal price)
+        {
+            _name = name;
+            _price = price;
+        }
+
+        public int Qty;
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public decimal Price
+        {
+            get { return _price; }
+            set
+            {
+                decimal oldPrice = _price;
+                _price = value;
+                if(PriceChanged!=null)
+                {
+                    PriceChanged(oldPrice, value);
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            return _name + " is priced at " + _price + ".";
+        }
+    }
+}
---
+++ WPF and Event Driven Programming/Inventory/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Trace the output of this code. \n");
+            Product saleProduct = new Product();
+  
+            //saleProduct.
+            saleProduct.PriceChanged += Program.PriceChanged;
+            saleProduct.Price = 30;
+            Console.WriteLine(saleProduct.ToString());
+            saleProduct = null; //releases for garbage collection
+
+            saleProduct = new Product("Bat");
+            saleProduct.PriceChanged += Program.PriceChanged;
+            Console.WriteLine(saleProduct.ToString());
+            saleProduct = null; //releases for garbage collection
+
+            saleProduct = new Product("Bike",100);
+            saleProduct.PriceChanged += Program.PriceChanged;
+            Console.WriteLine(saleProduct.ToString());
+            saleProduct = null; //releases for garbage collection
+
+            saleProduct = new Product("Ball", 10);
+            saleProduct.Name = "Widget";
+            saleProduct.PriceChanged += Program.PriceChanged;
+            saleProduct.Price = 20;
+            Console.WriteLine(saleProduct.ToString());
+            saleProduct = null; //releases for garbage collection
+
+            Console.WriteLine("Press any key to continue.");
+            Console.ReadLine();
+        }
+
+        public static void PriceChanged(decimal oldPrice, decimal newPrice)
+        {
+            //Notice the string formatting:
+            Console.WriteLine("I detected the price changed. Price went from {0} to {1}.", oldPrice, newPrice );
+        }
+    }
+}
