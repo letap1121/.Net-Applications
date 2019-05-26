36672580fc30f5bf2ded34f9d6f08a94751094f7
letap1121 <sagar2332@icloud.com>
2019-05-26 14:03:51 -0500

    Add files via upload

---
+++ Collection Classes in C#  and More WPF/StackExample/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             //Trace the output before running it
+
+            //Initialize Stack
+            Stack<string> stack = new Stack<string>();
+            //Add Item to Stack
+            stack.Push("A");
+            stack.Push("B");
+            stack.Push("C");
+            //Remove item from Stack
+            Console.WriteLine(stack.Pop());
+            stack.Push("D");
+            Console.WriteLine(stack.Peek());
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            stack.Push("E");
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+
+            Console.ReadLine();
+        }
+    }
+}
